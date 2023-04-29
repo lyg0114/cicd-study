0d93bd00155223ed8edbbe2e8a85ffcedaeb4064
lyg0114 <whdnseowkd@naver.com>
2023-04-29 10:15:07 +0900

    add sampleData

--- src/main/java/com/lizcal/springapi/SpringApiApplication.java
+++ src/main/java/com/lizcal/springapi/SpringApiApplication.java
@@ -1,7 +1,11 @@
 package com.lizcal.springapi;
 
+import com.lizcal.springapi.entity.ThemeParkRide;
+import com.lizcal.springapi.repository.ThemeParkRideRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringApiApplication {
@@ -10,4 +14,13 @@ public class SpringApiApplication {
     SpringApplication.run(SpringApiApplication.class, args);
   }
 
+  @Bean
+  public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+    return (args) -> {
+      repository
+          .save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+      repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+      repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+    };
+  }
 }
