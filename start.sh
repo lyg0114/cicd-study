2d2ff51ebe69575b9218bce6f17d28f1d6ac82ee
lyg0114 <whdnseowkd@naver.com>
2023-04-29 16:04:30 +0900

    modify : start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-docker rmi my-spring-app
-docker build -t my-spring-app .
-docker run -p 8080:8080 my-spring-app
+docker build -t my-spring-app .;
+docker rm --force my-app;
+docker run -p 8080:8080 --name my-app my-spring-app;
